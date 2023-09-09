ca2b49d6f6f5547ee74099f9b9e74282d90feec8
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-09-09 09:33:57 -0700

    NOQE

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This program prints alphabetic from a to z except q and e.
+ * and returns.
+ *
+ * Return: 0 upon successful execution.
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	
+	{
+	
+		if (letter == 'q' || letter == 'e')
+	{
+		continue;
+	}
+		putchar(letter);
+	}
+		putchar('\n');
+	return (0);
+}
