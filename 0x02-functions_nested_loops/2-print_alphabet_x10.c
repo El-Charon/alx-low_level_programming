e9e0600ee2fb71985589134cd92951d5f42791c1
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-09-13 08:52:47 -0700

    Lower

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int letter;
+
+	int count = 0;
+
+	while (count < 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(n);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
