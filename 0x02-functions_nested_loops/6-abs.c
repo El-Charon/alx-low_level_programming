1b57ce169e59e9e68e430a3c77ed8ccc817b0e63
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-09-13 09:06:30 -0700

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _abs - prints the absolute value of a number
+*@ab: number targeted
+*Return: returns 0
+*/
+
+int _abs(int ab)
+{
+if (ab > 0)
+return (ab);
+else if (ab < 0)
+return (-ab);
+else
+return (0);
+}
