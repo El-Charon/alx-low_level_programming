398eedf5e43a25c00de95669daab080632fa6a67
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-10-13 15:03:51 -0700

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,6 +6,28 @@
  * Return: the int converted from the string
  */
 
+int _atoi(char *s)
+{
+    int result = 0;
+    int i = 0;
+    int sign = 1; // Initialize to positive
+
+    // Check for negative sign
+    if (s[0] == '-')
+    {
+        sign = -1;
+        i = 1; // Skip the negative sign
+    }
+
+    // Iterate through the string and convert to an integer
+    for (; s[i] != '\0'; ++i)
+    {
+        result = result * 10 + (s[i] - '0');
+    }
+
+    return result * sign;
+}
+
 int main(int argc, char *argv[])
 {
 	int a, b;
