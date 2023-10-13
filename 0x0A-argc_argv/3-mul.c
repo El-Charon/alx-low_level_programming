a5340b2f6877c6c1d54bd2fe7f04d151d5020e82
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-10-13 15:40:04 -0700

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
- */
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	int a, b;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	a = _atoi(argv[1]);
-	b = _atoi(argv[2]);
-	printf("%d\n", a * b);
-	return (0);
+    // Convert the command-line arguments to integers
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+
+    // Multiply the numbers
+    int result = num1 * num2;
+
+    // Print the result
+    printf("%d\n", result);
+
+    return 0;
 }
