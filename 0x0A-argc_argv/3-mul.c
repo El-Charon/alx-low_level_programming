12ff37ce45c67a6bda1eda440221a1e434092434
El-Charon <70296200+El-Charon@users.noreply.github.com>
2023-10-13 15:53:48 -0700

    test

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+	
     if (argc != 3) {
         printf("Error\n");
         return 1;
     }
 
-    // Convert the command-line arguments to integers
     int num1 = atoi(argv[1]);
     int num2 = atoi(argv[2]);
 
-    // Multiply the numbers
     int result = num1 * num2;
 
-    // Print the result
     printf("%d\n", result);
 
     return 0;
